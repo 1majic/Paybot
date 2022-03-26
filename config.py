09acacb657256daf42498f5d7c8f27494df10c16
Dinislam Nizamov <58144991+1majic@users.noreply.github.com>
2022-03-26 14:29:47 +0500

    Windows reinstall 26.03.2022

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = '5083989196:AAEUc9o_w6-XrZciNOsEE6sIMKcT1ZfFa4U'  # sdfdsfscrtvervwrvwbot
+# TOKEN = '5017900389:AAH3OGvoUm5AE05ABkK6NvMA3jxKBLJzaJw'    # tsethellbot
+admins_id = [416702541, 1800414712]
