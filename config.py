a2d6d7645cbe5544e99063637f0b1c2a8503c547
Dinislam Nizamov <58144991+1majic@users.noreply.github.com>
2022-03-28 11:23:02 +0500

    12

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-TOKEN = '5083989196:AAEUc9o_w6-XrZciNOsEE6sIMKcT1ZfFa4U'  # sdfdsfscrtvervwrvwbot
-# TOKEN = '5017900389:AAH3OGvoUm5AE05ABkK6NvMA3jxKBLJzaJw'    # tsethellbot
+TOKEN = ''
 admins_id = [416702541, 1800414712]
